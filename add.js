d9318393c42f8a0e97bff0ddb672965cf76fc668
eduardsnovosads <eduardixx007@gmail.com>
2021-03-09 15:58:07 +0200

    11-stunda

--- add.js
+++ add.js
@@ -4,8 +4,21 @@ const list = document.querySelector('#book-list ul');
 list.addEventListener('click', (e) => {
     if(e.target.className == 'delete'){
       const li = e.target.parentElement;
-      
-      
       li.parentNode.removeChild(li);
     }
   });
+  
+const forms = document.forms;
+console.log(forms);
+console.log(forms['add-book']);
+
+Array.from(forms).forEach(function(form){
+  console.log(form);
+});
+
+const addForm = forms['add-book'];
+addForm.addEventListener('submit', function(e){
+  e.preventDefault();
+  const value = addForm.querySelector('input[type="text"]').value;
+  console.log(value);
+});
